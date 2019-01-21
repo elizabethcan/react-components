c0addde40a7dbef92e5d55215f344abfcbd044f2
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-21 15:24:18 -0800

    Retype of Todo List to learn

--- app.js
+++ app.js
@@ -1,20 +1,77 @@
-// var App = () => (
-//   <div>Some cliche salutation</div>
+// var GroceryListItem = (props) => {
+
+//   var onListItemClick = (event) => {
+//     console.log('I got clicked');
+//   }
+
+//   return (
+//   <ul>
+//     <li onClick={onListItemClick}>{props.groceryItems[0]}</li>
+//     <li>{props.groceryItems[1]}</li>
+//   </ul>
+// )};
+
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render () {
+//     return (
+//       <li>{this.props.groceryItems}</li>
+//     );
+//   }
+// }
+
+// var GroceryList = () => (
+//   <ul>
+//     {/* <GroceryListItem groceryItems = {['apples','oranges']} /> */}
+//     {props.groceryListItem.map(item =>
+//       <GroceryListItem item={item} />
+//     )}
+//   </ul>
 // );
 
-// ReactDOM.render(<App />, document.getElementById("app"));
+// ReactDOM.render(<GroceryList />, document.getElementById("app"));
 
-var GroceryList = () => (
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    }
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+    );
+  }
+}
+
+var TodoList = (props) => (
   <ul>
-    <GroceryListItem items = {['apples','oranges']} />
+    {props.todos.map(todo =>
+      <TodoListItem todo={todo} />
+    )}
   </ul>
 );
 
-var GroceryListItem = (props) => (
-  <ul>
-    <li>{props.items[0]}</li>
-    <li>{props.items[1]}</li>
-  </ul>
-)
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+  </div>
+);
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
